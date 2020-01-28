c7ff92e56715bbe646f4299cc8a0952aa310c0d8
Manoj Naidu <manojnaidu619@gmail.com>
2020-01-28 18:55:11 +0530

    fixed serialize-js

--- src/containers/App1.js
+++ src/containers/App1.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react'
 
 class App1 extends Component{
+
+  componentDidMount(){
+    console.log("from App1 componentDidMount")
+  }
+
   render(){
+    console.log("From App1 Render")
     return(
       <h2>From App1</h2>
     )
