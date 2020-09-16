a175e59fcf8e6e5d8a476793a3b6fa0f9c652e5c
Manoj Naidu <manojnaidu619@gmail.com>
2020-09-16 22:10:02 +0530

    Update alpha.py

--- scripts/alpha.py
+++ scripts/alpha.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import pandas
+import pandas, os
 from datetime import datetime
 from alpha_vantage.timeseries import TimeSeries
 
@@ -7,7 +7,7 @@ from alpha_vantage.timeseries import TimeSeries
 # now = datetime.now()
 # print(now.strftime("%b %d, %Y(%H:%M:%S)"))
 
-ts = TimeSeries('TPQBCYY9DA1F6X6U', output_format='pandas', indexing_type='date')
+ts = TimeSeries(os.environ['ALPHA_VANTAGE_KEY'], output_format='pandas', indexing_type='date')
 symbol = 'DXYN'
 
 values, columns = ts.get_daily(symbol=symbol, outputsize='full')
